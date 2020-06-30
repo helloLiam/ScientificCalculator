8ef8ed488086bdebfeea77b5a16d6a162a2f07a2
hellomop <41988525+hellomop@users.noreply.github.com>
2020-06-30 21:52:46 +0800

    Add

---
+++ ScientificCalculator0630/ScientificCalculator0630/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace ScientificCalculator0630
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.result = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.buttonEquals = new System.Windows.Forms.Button();
+            this.buttonDivide = new System.Windows.Forms.Button();
+            this.buttonMul = new System.Windows.Forms.Button();
+            this.buttonSub = new System.Windows.Forms.Button();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.num2 = new System.Windows.Forms.TextBox();
+            this.num1 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(464, 383);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.result);
+            this.panel4.Location = new System.Drawing.Point(28, 97);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(393, 53);
+            this.panel4.TabIndex = 3;
+            // 
+            // result
+            // 
+            this.result.AutoSize = true;
+            this.result.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.result.Location = new System.Drawing.Point(0, 9);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(85, 31);
+            this.result.TabIndex = 2;
+            this.result.Text = "Result";
+            this.result.Click += new System.EventHandler(this.result_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.buttonEquals);
+            this.panel3.Controls.Add(this.buttonDivide);
+            this.panel3.Controls.Add(this.buttonMul);
+            this.panel3.Controls.Add(this.buttonSub);
+            this.panel3.Controls.Add(this.buttonAdd);
+            this.panel3.Location = new System.Drawing.Point(28, 171);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(393, 193);
+            this.panel3.TabIndex = 1;
+            this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint);
+            // 
+            // buttonEquals
+            // 
+            this.buttonEquals.Location = new System.Drawing.Point(305, 130);
+            this.buttonEquals.Name = "buttonEquals";
+            this.buttonEquals.Size = new System.Drawing.Size(75, 23);
+            this.buttonEquals.TabIndex = 4;
+            this.buttonEquals.Text = "=";
+            this.buttonEquals.UseVisualStyleBackColor = true;
+            // 
+            // buttonDivide
+            // 
+            this.buttonDivide.Location = new System.Drawing.Point(305, 14);
+            this.buttonDivide.Name = "buttonDivide";
+            this.buttonDivide.Size = new System.Drawing.Size(75, 23);
+            this.buttonDivide.TabIndex = 3;
+            this.buttonDivide.Text = "÷";
+            this.buttonDivide.UseVisualStyleBackColor = true;
+            this.buttonDivide.Click += new System.EventHandler(this.buttonDivide_Click);
+            // 
+            // buttonMul
+            // 
+            this.buttonMul.Location = new System.Drawing.Point(305, 43);
+            this.buttonMul.Name = "buttonMul";
+            this.buttonMul.Size = new System.Drawing.Size(75, 23);
+            this.buttonMul.TabIndex = 2;
+            this.buttonMul.Text = "x";
+            this.buttonMul.UseVisualStyleBackColor = true;
+            this.buttonMul.Click += new System.EventHandler(this.buttonMul_Click);
+            // 
+            // buttonSub
+            // 
+            this.buttonSub.Location = new System.Drawing.Point(305, 72);
+            this.buttonSub.Name = "buttonSub";
+            this.buttonSub.Size = new System.Drawing.Size(75, 23);
+            this.buttonSub.TabIndex = 1;
+            this.buttonSub.Text = "-";
+            this.buttonSub.UseVisualStyleBackColor = true;
+            this.buttonSub.Click += new System.EventHandler(this.buttonSub_Click);
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(305, 101);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 0;
+            this.buttonAdd.Text = "+";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.num2);
+            this.panel2.Controls.Add(this.num1);
+            this.panel2.Location = new System.Drawing.Point(28, 15);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(393, 63);
+            this.panel2.TabIndex = 0;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // num2
+            // 
+            this.num2.Location = new System.Drawing.Point(12, 29);
+            this.num2.Name = "num2";
+            this.num2.Size = new System.Drawing.Size(368, 23);
+            this.num2.TabIndex = 1;
+            this.num2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // num1
+            // 
+            this.num1.Location = new System.Drawing.Point(12, 0);
+            this.num1.Name = "num1";
+            this.num1.Size = new System.Drawing.Size(368, 23);
+            this.num1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(649, 507);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "ScientificCalculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button buttonSub;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button buttonEquals;
+        private System.Windows.Forms.Button buttonDivide;
+        private System.Windows.Forms.Button buttonMul;
+        private System.Windows.Forms.TextBox num2;
+        private System.Windows.Forms.TextBox num1;
+        private System.Windows.Forms.Label result;
+        private System.Windows.Forms.Panel panel4;
+    }
+}
+
---
+++ ScientificCalculator0630/ScientificCalculator0630/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScientificCalculator0630
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            var number1 = int.Parse(this.num1.Text);
+            var number2 = int.Parse(this.num2.Text);
+            var finalResult = number1 + number2;
+            this.result.Text = finalResult.ToString();
+        }
+
+        private void result_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonSub_Click(object sender, EventArgs e)
+        {
+            var number1 = int.Parse(this.num1.Text);
+            var number2 = int.Parse(this.num2.Text);
+            var finalResult = number1 - number2;
+            this.result.Text = finalResult.ToString();
+        }
+
+        private void buttonMul_Click(object sender, EventArgs e)
+        {
+            var number1 = int.Parse(this.num1.Text);
+            var number2 = int.Parse(this.num2.Text);
+            var finalResult = number1 * number2;
+            this.result.Text = finalResult.ToString();
+        }
+
+        private void buttonDivide_Click(object sender, EventArgs e)
+        {
+            var number1 = int.Parse(this.num1.Text);
+            var number2 = int.Parse(this.num2.Text);
+            if (number2!=0)
+            {              
+                var finalResult = number1 + number2;
+                this.result.Text = finalResult.ToString();
+            }
+            else
+            {
+                this.result.Text = "被除数不能为 0";
+            }
+            
+        }
+    }
+}
